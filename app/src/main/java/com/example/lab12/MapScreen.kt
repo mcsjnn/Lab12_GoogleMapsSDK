fa6793fcc964f9bba3a2f0d048343cda5859adf8
Sujenn Maciel Fiorella Ancco Escobar <sujenn.ancco@tecsup.edu.pe>
2024-10-25 18:06:41 -0500

    Se agrego poligonos para 3 distritos

--- app/src/main/java/com/example/lab12/MapScreen.kt
+++ app/src/main/java/com/example/lab12/MapScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
@@ -15,6 +16,7 @@ import com.google.maps.android.compose.rememberCameraPositionState
 import com.google.maps.android.compose.rememberMarkerState
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.Polygon
 
 
 @Composable
@@ -61,6 +63,47 @@ fun MapScreen() {
                     durationMs = 3000
                 )
             }
+
+            val mallAventuraPolygon = listOf(
+                LatLng(-16.432292, -71.509145),
+                LatLng(-16.432757, -71.509626),
+                LatLng(-16.433013, -71.509310),
+                LatLng(-16.432566, -71.508853)
+            )
+
+
+            val parqueLambramaniPolygon = listOf(
+                LatLng(-16.422704, -71.530830),
+                LatLng(-16.422920, -71.531340),
+                LatLng(-16.423264, -71.531110),
+                LatLng(-16.423050, -71.530600)
+            )
+
+            val plazaDeArmasPolygon = listOf(
+                LatLng(-16.398866, -71.536961),
+                LatLng(-16.398744, -71.536529),
+                LatLng(-16.399178, -71.536289),
+                LatLng(-16.399299, -71.536721)
+            )
+
+            Polygon(
+                points = plazaDeArmasPolygon,
+                strokeColor = Color.Red,
+                fillColor = Color.Blue,
+                strokeWidth = 5f
+            )
+            Polygon(
+                points = parqueLambramaniPolygon,
+                strokeColor = Color.Red,
+                fillColor = Color.Blue,
+                strokeWidth = 5f
+            )
+            Polygon(
+                points = mallAventuraPolygon,
+                strokeColor = Color.Red,
+                fillColor = Color.Blue,
+                strokeWidth = 5f
+            )
         }
     }
 }
